f60e65dee1e5d5d12b39073853378caf49eec742
pablohs1986 <pabloherrero1986@gmail.com>
2021-03-18 11:42:36 +0100

    EJS engine implemented

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 const app = new express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.listen(4000, () => {
-    console.log('Express runninggggg jijuuu')
+    console.log('Express runninggggg yiiihaaaa')
+});
+
+app.get('/', (req,res) => {
+    res.render('index');
+});
+
+app.get('/about', (req,res) => {
+    res.render('about');
+});
+
+app.get('/post', (req,res) => {
+    res.render('post');
+});
+
+app.get('/contact', (req,res) => {
+    res.render('contact');
 });
