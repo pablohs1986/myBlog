e152e9e228194c33e324729a24ee5de9bf5b6707
pablohs1986 <pabloherrero1986@gmail.com>
2021-03-21 12:02:59 +0100

    user login, validation and logout

--- controllers/home.js
+++ controllers/home.js
@@ -3,6 +3,7 @@ const BlogPost = require('../models/BlogPost');
 module.exports = async (req,res) => { 
     console.log('>>> Home starting...')
     const blogposts = await BlogPost.find({});
+    console.log('>>> ' + req.session.userId);
     res.render('index', {
         blogposts
     });
--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const newPostController = require('./controllers/newPost');
 const homeController = require('./controllers/home');
 const storePostController = require('./controllers/storePost');
 const getPostController = require('./controllers/getPost');
+const newUserController = require('./controllers/newUser');
+const storeUserController = require('./controllers/storeUser');
+const loginController = require('./controllers/login');
+const loginUserController = require('./controllers/loginUser');
+const expressSession = require(`express-session`)
+const authMiddleware = require('./middleware/authMiddleWare');
+const redirectIfAuthenticatedMiddleware = require('./middleware/redirectIfAuthenticatedMiddleware');
+const logoutController = require('./controllers/logout');
+const { allowedNodeEnvironmentFlags } = require('process');
+global.loggedIn = null;
 const app = new express();
 
 ////// Mongo connection //////
@@ -23,16 +33,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(fileUpload());
 app.use('/posts/store', validateMiddleWare);
+app.use(expressSession({secret: 'keyboard cat'}));
+app.use("*", (req, res, next) => {
+    loggedIn = req.session.userId;
+    next();
+});
 
 app.listen(4000, () => {
     console.log('Express RRRUUUUNNNIIIIINNNNGGGG!!!!!!');
 });
 
-////// Gets //////
+////// Gets & Posts //////
 app.get('/', homeController);
 app.get('/post/:id', getPostController);
-app.get('/posts/new', newPostController);
-app.post('/posts/store', storePostController);
+app.get('/posts/new', authMiddleware, newPostController);
+app.get('/auth/register', redirectIfAuthenticatedMiddleware, newUserController);
+app.get('/auth/login', redirectIfAuthenticatedMiddleware, loginController);
+app.get('/auth/logout', logoutController);
+app.post('/posts/store', authMiddleware, storePostController);
+app.post('/users/register', redirectIfAuthenticatedMiddleware, storeUserController);
+app.post('/users/login', redirectIfAuthenticatedMiddleware, loginUserController);
+
+// Not found
+app.use((req,res) => res.render('notfound'));
 
 
 
